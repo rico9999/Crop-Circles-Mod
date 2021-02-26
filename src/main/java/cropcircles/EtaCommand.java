bc9852cde9fa027d658f4800d137d178624123f5
Eric Anderson <eta@Palladium.local>
2021-02-26 00:14:48 -0800

    Exercise #1 by default.

--- src/main/java/cropcircles/EtaCommand.java
+++ src/main/java/cropcircles/EtaCommand.java
@@ -143,10 +143,10 @@ public class EtaCommand {
 		  int block_count = 0;
 		  
 		  // Exercises
-		  boolean do_exercise1 = false;
+		  boolean do_exercise1 = true;
 		  boolean do_exercise2 = false;
 		  boolean do_exercise3 = false;
-		  boolean do_exercise4 = true;
+		  boolean do_exercise4 = false;
 		  
 		  
 		  // Nested for loops
